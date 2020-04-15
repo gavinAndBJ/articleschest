45d3dec412714688a91c81e3257b8c59c7d3a173
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-04-15 17:13:41 -0400

    implemented- Display data

--- script.js
+++ script.js
@@ -7,33 +7,66 @@ articleApp.apiKey = `uXiSZXYADR9VvLuUkUqDWxnFIzAtZpy6`;
 articleApp.apiUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?';
 
 
+const $searchInput = $('#search');
+const $searchForm = $('#target');
+const $articleHeading = $('.heading');
+const $articleAuthor = $('.author');
+const $articleDescription = $('.description');
+const $articleDate = $('.date');
+const $articleWordCount = $('.wordCount');
+
+console.log($searchInput)
 
 // Connect to API using AJAX
-articleApp.getArticles = () => {
+articleApp.getArticles = (value) => {
+    console.log(value);
     $.ajax({
         url: articleApp.apiUrl,
         method: `GET`,
         dataType: `json`,
         data: {
-            q: 'food',
+            // Use userInput variable to search NYTimes API
+            q: value,
             "api-key": articleApp.apiKey,
         }
     }).then((res) => {
         console.log(res);
-        // articleApp.addDetails(res);
+        // for(let i = 0; i<=2; i++ ){
+            
+        // }
+        articleApp.displayArticle(res);
+
+
     })
 }
 
+articleApp.userInput = () => {
+    $searchForm.submit((event) => {
+        // alert("Handler for .submit() called.");
+        event.preventDefault();
+        // Create a variable to accept user input from search bar
+        const value = $searchInput.val();
+        console.log(value);
 
+        articleApp.getArticles(value);
+    });
 
-// Create a variable to accept user input from search bar
-// Use userInput variable to search NYTimes API
+}
+
 // Display results (Header / Description / Photo)
+articleApp.displayArticle = (response) => {
+    $articleHeading.text(`${response.response.docs[0].headline.main}`);
+    $articleAuthor.text(`${response.response.docs[0].byline.original}`)
+    $articleDate.text(`${response.response.docs[0].pub_date}`)
+    $articleWordCount.text(`${response.response.docs[0].word_count}`)
+    $articleDescription.text(`${response.response.docs[0].abstract}`)
+}
 
 
 // Create init
 articleApp.init = () => {
-    articleApp.getArticles();
+    // articleApp.getArticles();
+    articleApp.userInput();
 }
 
 // Create document ready
