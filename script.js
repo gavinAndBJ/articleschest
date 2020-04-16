9c4efe9e028db467445bde0271040d9a254fa0df
Gavin Sutton <gavin.w.c.sutton@gmail.com>
2020-04-16 19:58:05 -0400

    Big update. Stock photo/Images Sized/Flex/Search reset/Filters via option select/Read more

--- script.js
+++ script.js
@@ -16,7 +16,13 @@ const $articleDate = $('.date');
 const $articleWordCount = $('.wordCount');
 
 // Connect to API using AJAX
-articleApp.getArticles = (value) => {
+articleApp.getArticles = (value, filter) => {
+    if (filter != `none` ) {
+        articleApp.filter = `news_desk:${filter}`;
+    } else {
+        articleApp.filter = ``;
+    }
+
     $.ajax({
         url: articleApp.apiUrl,
         method: `GET`,
@@ -24,6 +30,7 @@ articleApp.getArticles = (value) => {
         data: {
             // Use userInput variable to search NYTimes API
             q: value,
+            fq: articleApp.filter,
             "api-key": articleApp.apiKey,
         }
     }).then((res) => {
@@ -33,11 +40,17 @@ articleApp.getArticles = (value) => {
 
 articleApp.userInput = () => {
     $searchForm.submit((event) => {
+        
+        $(`.articlePost`).empty();
         // alert("Handler for .submit() called.");
         event.preventDefault();
         // Create a variable to accept user input from search bar
         const value = $searchInput.val();
-        articleApp.getArticles(value);
+        const filter = $(`option:selected`).val();
+        console.log(filter)
+        articleApp.getArticles(value, filter);
+        $(`.searchQuery`).html(`<h2>Your search for "${value}"...</h2>`);
+        $searchInput.val(``);
     });
 
 }
@@ -52,24 +65,43 @@ articleApp.displayArticle = (response) => {
     let articleInfo = response.response.docs;
     // console.log(articleInfo)
     articleInfo.forEach((article)=>{
-        console.log(`${ article.pub_date}`)
         const date = articleApp.formatDate(`${article.pub_date}`)
-        console.log(date);
+        console.log(article)
+        console.log(article.word_count)
+        
+        if (article.multimedia.length > 0) {
+            articleApp.image = `https://www.nytimes.com/${article.multimedia[0].url}`
+        } else { 
+            articleApp.image = `./assets/placeholder.png`
+        }
+
+        if (article.byline.original != null) {
+            articleApp.author = article.byline.original
+        } else {
+            articleApp.author = `No Listed Author`
+        }
+
+        if (article.word_count = 0) {
+            articleApp.WordCount = article.word_count
+        } else {
+            articleApp.WordCount = `Not Listed`
+        }
+
+
         const htmlToAppend = `
-        <div class="wrapper">
             <div class="articleContainer">
                 <div class="image">
-                    <img src="https://www.nytimes.com/${article.multimedia[19].url}" alt="">
+                    <img src="${articleApp.image}" alt="">
                 </div>
                 <div class="details">
-                    <h2>${article.headline.main}</h2>
-                    <p>${article.byline.original}</p>
-                    <p>${date}</p>
-                    <p>${article.word_count}</p>
+                    <h3>${article.headline.main}</h3>
+                    <p>${articleApp.author}</p>
+                    <p><strong>Date Published:</strong> ${date}</p>
+                    <p><strong>Word Count:</strong> ${articleApp.WordCount}</p>
                     <p>${article.abstract}</p>
+                    <p><a href="${article.web_url}">Read More</a></p>
                 </div>
             </div>
-        </div>
         `;
 
         $(`.articlePost`).append(htmlToAppend)
